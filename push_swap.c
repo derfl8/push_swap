616bb3159a4e7338269f4b6020636847bcb59f5c
derfl8 <alfred.begou@gmail.com>
2026-03-09 19:02:48 +0100

    fix parsing for char -

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 15:50:08 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/09 17:59:34 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/09 19:01:59 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 static int	argcheck(char *arg)
 {
-	while (*arg)
+	int	i;
+	
+	i = 0;
+	while (arg[i])
 	{
-		if (*arg != ' ' && *arg != '-' && (*arg < '0' || *arg > '9'))
+		if (arg[i] != ' ' && arg[i] != '-' && (arg[i] < '0' || arg[i] > '9'))
 			return (1);
-		arg++;
+		else if (arg[i] == '-'
+			&& (arg[i + 1] == ' ' || arg[i + 1] == '-' || arg[i + 1] == '\0'))
+			return (1);
+		else if (arg[i] == '-' && (arg[i - 1] >= '0' && arg[i - 1] <= '9'))
+			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -29,7 +37,7 @@ int	main(int ac, char **av)
 	
 	if (ac < 2)
 	{
-		ft_printf("Give some numbers\n");
+		write(2, "Error\n", 6);
 		return (0);
 	}
 	while (--ac > 0)
@@ -37,7 +45,7 @@ int	main(int ac, char **av)
 		valid_or_not = argcheck(av[ac]);
 		if (valid_or_not == 1)
 		{
-			ft_printf("Wrong argument(s)\n");
+			write(2, "Error\n", 6);
 			return (0);
 		}
 	}
