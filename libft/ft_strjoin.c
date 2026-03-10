6a4bbde08bb3404803f7f44fc317812caa9feaca
derfl8 <alfred.begou@gmail.com>
2026-03-10 19:04:17 +0100

    test

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abegou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 18:18:44 by abegou            #+#    #+#             */
-/*   Updated: 2025/11/10 19:58:25 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/10 19:04:04 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ret;
-	int		j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	ret = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	ret = ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (ret == NULL)
 		return (NULL);
-	j = -1;
-	while (s1[++j])
-		ret[j] = s1[j];
-	ret[j] = '\0';
-	j = -1;
-	while (s2[++j])
-		ret[ft_strlen(s1) + j] = s2[j];
-	ret[ft_strlen(s1) + j] = '\0';
+	ft_strlcpy(ret, s1, ft_strlen(s1) + 1);
+	ft_strlcpy(&ret[ft_strlen(s1) + 1], s2, ft_strlen(s2) + 1);
 	return (ret);
 }
