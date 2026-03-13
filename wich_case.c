09d3795c2beb2b6336ddc46b0e83ac1d32b8a6e8
derfl8 <alfred.begou@gmail.com>
2026-03-13 16:08:20 +0100

    able to edit stack

--- wich_case.c
+++ wich_case.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 22:23:28 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/13 16:06:35 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/13 16:07:55 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,10 @@ void	wich_case(t_tab **a)
 	if (already_sort(*a) == true)
 		return ;
 	if (how_many(*a) == 3)
-		three_sort(*a);
+		three_sort(a);
 	else if (how_many(*a) == 5)
-		five_sort(*a);
+		five_sort(a);
 	else
-		radix(*a);
+		radix(a);
 	return ;
 }
